97c08578a346f069c3f7e9cbf76bca7d21c2577d
Yazan Alshrouf <yazanalshrouf@gmail.com>
2022-03-11 20:10:20 +0300

    implemented chat api

--- src/controllers/chat.js
+++ src/controllers/chat.js
@@ -1,12 +1,39 @@
-import formidable from 'formidable';
-const form = new formidable.IncomingForm();
+const processData = fileData => {
+	const msgs = fileData.toString('utf-8').split(/\r\n|\n|\r/);
 
-const chat = function(req, res) {
-    form.parse(req, function(err, fields, files){
-        console.log(files);
-    });
-    console.log('after');
-    res.end('hi');
-}
+	const pattern = /^\[?(\d+[./]\d+[./]\d+),\s+[^\]-]+[\]-]?\s([^:]+):\s*(.*)/;
+
+	const data = [];
+
+	let prevday;
+	msgs.forEach(line => {
+		const match = pattern.exec(line);
+		if (!match) return;
+		const [, date, name] = match;
+
+		if (prevday && prevday.date === date) {
+			if (name in prevday.recipients) prevday.recipients[name] += 1;
+			else prevday.recipients[name] = 1;
+		} else {
+			prevday = {
+				date,
+				recipients: {},
+			};
+			prevday.recipients[name] = 1;
+			data.push(prevday);
+		}
+	});
+	return data;
+};
+
+// const parseFile = stream => {};
+
+const chat = (req, res) => {
+	const data = processData(req.files.chat.data);
+	res.json({
+		status: 'success',
+		data,
+	});
+};
 
 export default chat;
--- src/routes/v1/chat/chat.js
+++ src/routes/v1/chat/chat.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import fileUpload from 'express-fileupload';
 
 import chat from '../../../controllers/chat.js';
 
 const router = express.Router();
 
-router.post('/chat', chat);
+router.use(fileUpload()).post('/chat', chat);
 
 export default router;
