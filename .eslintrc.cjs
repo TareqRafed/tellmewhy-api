09cb6ee8eeba03e4558ed46b5e0954bc28aa2b5a
Yazan Alshrouf <yazanalshrouf@gmail.com>
2022-03-11 12:43:46 +0300

    prettier now imports Eslit config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,15 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['airbnb-base', 'plugin:prettier/recommended'],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+	},
 };
