633a971f23ad90c2c1c25a93d65d975a4146fc12
TareqRafed <tareq.rafed22@gmail.com>
2022-03-16 00:29:03 +0300

    refactor: variables between middlewares, filenames and imports

--- src/controllers/handleLogin.js
+++ src/controllers/handleLogin.js
@@ -1,22 +1,27 @@
 const generateToken = (email, password) => {
+    // TODO: Implement JWT
     return `${email} ${password} ${Math.random()}`
 }
 
 const handleLogin = (req, res, next) => {
-    const email = req.body?.email
-    const password = req.body?.password
+    const { email } = req.body || {}
+    const { password } = req.body || {}
 
     if (!email || !password) {
-        res.status(401).send({
-            message: 'Missing Field',
-        })
+        res.status(401)
+        res.locals = {
+            message: 'Email or Password is incorrect',
+            data: null,
+            sucess: false,
+        }
+        next()
     }
-
-    next({
+    res.locals = {
         message: 'User logged in successfuly',
         data: { email, token: generateToken(email, password) },
         success: true,
-    })
+    }
+    next()
 }
 
 export default handleLogin
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
 
 import chat from './chat/index.js'
+import admin from './admin/index.js'
 
 const router = express.Router()
 
-router.use('/api/v1', chat)
+router.use('/api/v1', chat, admin)
 
 export default router
